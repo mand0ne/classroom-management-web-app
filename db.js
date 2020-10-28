f4a2ebfe0cdbc436ddc88bf988ca9ace23538d9c
mand0ne <amandal1@etf.unsa.ba>
2020-10-28 15:36:58 +0100

    add screenshots

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("dbwt19", "root", "root", {
+const sequelize = new Sequelize("dbwt19", "root", "", {
     host: "localhost",
     dialect: "mysql",
     logging: false,
